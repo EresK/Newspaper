f8d5e769896ac96e8416c503297800fd4e40bae6
EresK <e.kuular@g.nsu.ru>
2022-11-22 23:54:56 +0700

    add security config

---
+++ backend/src/main/java/com/newspaper/backend/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.newspaper.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails userAdmin = User.withUsername("userAdmin")
+                .password(passwordEncoder().encode("adminAdmin"))
+                .roles("ADMIN", "USER")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("userUser"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userAdmin, user);
+    }
+
+    // @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
