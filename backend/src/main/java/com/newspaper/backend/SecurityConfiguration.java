c44c8e1c243b9a26c8d8f345d7c0185ebe2c4c53
EresK <e.kuular@g.nsu.ru>
2022-11-24 21:14:25 +0700

    fix security conf

--- backend/src/main/java/com/newspaper/backend/SecurityConfiguration.java
+++ backend/src/main/java/com/newspaper/backend/SecurityConfiguration.java
@@ -16,28 +16,32 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails userAdmin = User.withUsername("userAdmin")
-                .password(passwordEncoder().encode("adminAdmin"))
+        UserDetails userAdmin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
                 .roles("ADMIN", "USER")
                 .build();
         UserDetails user = User.withUsername("user")
-                .password(passwordEncoder().encode("userUser"))
+                .password(passwordEncoder().encode("user"))
                 .roles("USER")
                 .build();
 
         return new InMemoryUserDetailsManager(userAdmin, user);
     }
 
-    // @Bean
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .authorizeHttpRequests()
-                .anyRequest()
-                .authenticated()
+                .cors().disable()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/publications").hasRole("ADMIN")
+                .antMatchers("/users").authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll();
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll()
+                .and()
+                .httpBasic();
 
         return httpSecurity.build();
     }
