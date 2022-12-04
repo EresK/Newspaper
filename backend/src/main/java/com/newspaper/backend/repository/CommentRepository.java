1cb964023a6a1dae53ccd280fc84219f68f20ff5
EresK <e.kuular@g.nsu.ru>
2022-12-04 13:56:23 +0700

    sort by packages

--- backend/src/main/java/com/newspaper/backend/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.newspaper.backend.repository;
-
-import com.newspaper.backend.entity.CommentEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends CrudRepository<CommentEntity, Long> {
-}
