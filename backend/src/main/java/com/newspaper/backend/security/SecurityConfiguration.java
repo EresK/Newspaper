94d4168381416070ba4e3aef8a44c7d2fde6ff49
EresK <e.kuular@g.nsu.ru>
2022-12-04 13:54:01 +0700

    just formatting

--- backend/src/main/java/com/newspaper/backend/security/SecurityConfiguration.java
+++ backend/src/main/java/com/newspaper/backend/security/SecurityConfiguration.java
@@ -1,46 +1,29 @@
 package com.newspaper.backend.security;
-import com.newspaper.backend.services.UserService;
+
+import com.newspaper.backend.user.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@AllArgsConstructor
 @Configuration
 @EnableWebSecurity
-@AllArgsConstructor
 public class SecurityConfiguration {
     private final UserService userService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsManager() {
-//        UserDetails admin = User.withUsername("admin")
-//                .password(passwordEncoder().encode("admin"))
-//                .roles("ADMIN")
-//                .build();
-//        UserDetails user = User.withUsername("user")
-//                .password(passwordEncoder().encode("user"))
-//                .roles("USER")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin, user);
-//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .cors().disable()
+        http.cors().disable()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/reg").permitAll()
-                .antMatchers("/publication").authenticated()
+                .antMatchers("/registration").permitAll()
+                .antMatchers("/publications").authenticated()
                 .antMatchers("/users").authenticated()
                 .and()
                 .formLogin().permitAll()
@@ -50,6 +33,7 @@ public class SecurityConfiguration {
                 .httpBasic();
         return http.build();
     }
+
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider(){
         DaoAuthenticationProvider provider=
