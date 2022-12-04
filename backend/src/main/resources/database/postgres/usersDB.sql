546f54263c2a01033603920afde5189f12def93a
EresK <e.kuular@g.nsu.ru>
2022-12-04 13:50:37 +0700

    del

--- backend/src/main/resources/database/postgres/usersDB.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (id, email, first_name, last_name)
-VALUES (1, 'admin@email.com', 'admin_name', 'admin_surname')
-ON CONFLICT DO NOTHING;
--- backend/src/main/resources/h_2/initDB.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id BIGINT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    UNIQUE(email)
-);
