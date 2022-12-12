03afe01e7d5916ccd6882be62ce2e04b0bfdeab5
EresK <e.kuular@g.nsu.ru>
2022-12-12 20:36:22 +0700

    add post put delete mappings & check for owner

--- backend/src/main/java/com/newspaper/backend/publication/PublicationController.java
+++ backend/src/main/java/com/newspaper/backend/publication/PublicationController.java
@@ -2,6 +2,9 @@ package com.newspaper.backend.publication;
 
 import com.newspaper.backend.description.DescriptionEntity;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -12,28 +15,39 @@ import java.util.Optional;
 public class PublicationController {
     private final PublicationService publicationService;
 
+    @PostMapping
+    public ResponseEntity<PublicationDto>
+    postPublication(Authentication auth, @RequestBody PublicationDto publicationDto) {
+        HttpStatus status = publicationService.createPublication(auth, publicationDto) ?
+                HttpStatus.CREATED : HttpStatus.NOT_MODIFIED;
+
+        return new ResponseEntity<>(publicationDto, status);
+    }
+
     @GetMapping
-    public Iterable<PublicationEntity> getPublications() {
-        return publicationService.getPublications();
+    public Iterable<PublicationEntity> getPublications(Authentication auth) {
+        return publicationService.getPublications(auth);
     }
 
     @GetMapping("/{id}")
-    public Optional<PublicationEntity> getPublicationById(@PathVariable Long id) {
-        return publicationService.getPublicationById(id);
+    public Optional<PublicationEntity> getPublicationById(Authentication auth, @PathVariable Long id) {
+        return publicationService.getPublicationById(auth, id);
     }
 
     @GetMapping("/{id}/description")
-    public Optional<DescriptionEntity> getDescription(@PathVariable Long id) {
-        return publicationService.getDescription(id);
+    public Optional<DescriptionEntity> getDescription(Authentication auth, @PathVariable Long id) {
+        return publicationService.getDescription(auth, id);
     }
 
-    // TODO: complete post mapping
-    // @PostMapping
-    public void postPublication() {
+    @PutMapping("{id}")
+    public void putPublicationById(Authentication auth,
+                                   @PathVariable Long id,
+                                   @RequestBody PublicationDto publicationDto) {
+        publicationService.updatePublication(auth, id, publicationDto);
     }
 
     @DeleteMapping("/{id}")
-    public void deletePublication(@PathVariable Long id) {
-        publicationService.deletePublication(id);
+    public void deletePublication(Authentication auth, @PathVariable Long id) {
+        publicationService.deletePublication(auth, id);
     }
 }
