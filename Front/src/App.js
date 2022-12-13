80069ce844597d35d0725c7785e30440ff9c6122
Ingvar-fesh <igor777.fesh@mail.ru>
2022-12-13 20:01:16 +0700

    Add components for page with articles

--- Front/src/App.js
+++ Front/src/App.js
@@ -2,38 +2,70 @@ import React, {useState} from 'react';
 import Post from "./components/Post";
 import Login from "./components/Login";
 import Registration from "./components/Registration";
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Router, Routes, Switch} from 'react-router-dom';
 import './styles/App.css'
 import PostsList from "./components/PostsList";
 import ListForm from "./components/ListForm";
 import TextEditor from "./components/TextEditor";
+import "./styles/TextEditor.css"
+import Articles from "./components/Articles";
+import "./styles/App.css"
 
 // import "./components/*"
 
 
-function App() {
-    const [someItem, setSomeItem] = useState([
-        {id: 0, title: 'Post 1', body: 'Content 1'},
-        {id: 1, title: 'Post 2', body: 'Content 2'},
-        {id: 2, title: 'Post 3', body: 'Content 3'}
-    ])
-
-    const addItem = (newItem) => {
-        setSomeItem([...someItem, newItem])
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+                {
+                    id: 1,
+                    title: "Post 1",
+                    img: "1_a.jpg",
+                    authors: "Tom",
+                    description: "New description",
+                },
+                {
+                    id: 2,
+                    title: "Post 2",
+                    img: "2_a.jpg",
+                    authors: "Peter",
+                    description: "New description",
+                },
+                {
+                    id: 3,
+                    title: "Post 3",
+                    img: "3_a.jpg",
+                    authors: "Anna",
+                    description: "New description",
+                }
+            ]
+        }
     }
-
-    return (
-        <div className="App">
-            {/*<Login/>*/}
-            {/*<Registration/>*/}
-            {/*<ListForm add={addItem}/>*/}
-            <PostsList posts={someItem} title={"Most popular publications"}/>
-            {/*<Post post = {{id: 1, title: 'test title', body: 'Content'}} />*/}
-            <div className="textEditorBlock">
-                <TextEditor />
+    render () {
+        return (
+            <div className="app">
+                <Articles items={this.state.items}/>
+                <Login/>
+                <Registration/>
+                {/*<Registration/>*/}
+                {/*<ListForm add={addItem}/>*/}
+                {/*<PostsList posts={someItem} title={"Most popular publications"}/>*/}
+                {/*<Post post = {{id: 1, title: 'test title', body: 'Content'}} />*/}
+                <div>
+                    <div>
+                        <h1>Описание статьи</h1>
+                        <h3>Название статьи:</h3>
+                        <h3>Авторы:</h3>
+                        <h3>Содержание:</h3>
+                        <h3>Дата публикации:</h3>
+                    </div>
+                    <TextEditor />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
