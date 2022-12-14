d0a666b9d26d1a6676e5805abb4e190b9b469140
Oleg Ivanov <o.ivanov@g.nsu.ru>
2022-12-14 09:02:07 +0700

    Create AdvertService.java

---
+++ backend/src/main/java/com/newspaper/backend/advert/AdvertService.java
@@ -0,0 +1,9 @@
+package com.newspaper.backend.advert;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class AdvertService {
+}
