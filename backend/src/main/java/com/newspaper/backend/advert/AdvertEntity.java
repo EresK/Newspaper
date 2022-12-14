34c14148c1f2937061d1ab1e0654a1f88c153f10
Oleg Ivanov <o.ivanov@g.nsu.ru>
2022-12-14 09:02:01 +0700

    Create AdvertEntity.java

---
+++ backend/src/main/java/com/newspaper/backend/advert/AdvertEntity.java
@@ -0,0 +1,42 @@
+package com.newspaper.backend.advert;
+
+import com.newspaper.backend.publication.PublicationEntity;
+import com.newspaper.backend.user.UserEntity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "adverts")
+public class AdvertEntity {
+    @SequenceGenerator(
+            name = "advert_sequence",
+            sequenceName = "advert_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "advert_sequence"
+    )
+    private Long id;
+    private String title;
+//    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+//            CascadeType.PERSIST, CascadeType.REFRESH})
+//    @JoinColumn(name = "", nullable = false)
+    private UserEntity advertiser;
+//    @OneToOne()
+    private PublicationEntity publication;
+
+    public AdvertEntity(Long id, String title, UserEntity advertiser, PublicationEntity publication) {
+        this.id = id;
+        this.title = title;
+        this.advertiser = advertiser;
+        this.publication = publication;
+    }
+// TODO: add adverts to all entites
+
+}
