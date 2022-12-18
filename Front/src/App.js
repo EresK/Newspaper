a363065f9132e53981e3761d6500336d8390e0be
lehad22 <lehad22002@gamil.com>
2022-12-19 06:19:32 +0700

    routing

--- Front/src/App.js
+++ Front/src/App.js
@@ -27,6 +27,61 @@ function App() {
             <AppRouter/>
         </div>
     );
+
+    //
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         items: [
+    //             {
+    //                 id: 1,
+    //                 title: "Post 1",
+    //                 img: "1_a.jpg",
+    //                 authors: "Tom",
+    //                 description: "New description",
+    //             },
+    //             {
+    //                 id: 2,
+    //                 title: "Post 2",
+    //                 img: "2_a.jpg",
+    //                 authors: "Peter",
+    //                 description: "New description",
+    //             },
+    //             {
+    //                 id: 3,
+    //                 title: "Post 3",
+    //                 img: "3_a.jpg",
+    //                 authors: "Anna",
+    //                 description: "New description",
+    //             }
+    //         ]
+    //     }
+    // }
+    // render () {
+    //     return (
+    //         <div className="app">
+    //             <Articles items={this.state.items}/>
+    //             <Login/>
+    //             <Registration/>
+    //             {/*<Registration/>*/}
+    //             {/*<ListForm add={addItem}/>*/}
+    //             {/*<PostsList posts={someItem} title={"Most popular publications"}/>*/}
+    //             {/*<Post post = {{id: 1, title: 'test title', body: 'Content'}} />*/}
+    //             <div>
+    //                 <div>
+    //                     <h1>Описание статьи</h1>
+    //                     <h3>Название статьи:</h3>
+    //                     <h3>Авторы:</h3>
+    //                     <h3>Содержание:</h3>
+    //                     <h3>Дата публикации:</h3>
+    //                 </div>
+    //                 <TextEditor />
+    //             </div>
+    //         </div>
+    //     );
+    // }
+
+
 }
 
 export default App;
