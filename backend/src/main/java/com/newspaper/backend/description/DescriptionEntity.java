939d5573c431cc0bbbc904e1505de3fd0a41d52a
EresK <e.kuular@g.nsu.ru>
2022-12-20 19:13:50 +0700

    add logic to repository & implement service

--- backend/src/main/java/com/newspaper/backend/description/DescriptionEntity.java
+++ backend/src/main/java/com/newspaper/backend/description/DescriptionEntity.java
@@ -1,5 +1,7 @@
 package com.newspaper.backend.description;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.newspaper.backend.publication.PublicationEntity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,34 +14,45 @@ import java.util.Date;
 @Entity
 @Table(name = "descriptions")
 public class DescriptionEntity {
-    @SequenceGenerator(
-            name = "description_sequence",
-            sequenceName = "description_sequence",
-            allocationSize = 1
-    )
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "description_sequence"
-    )
+    @GeneratedValue
     private Long id;
 
+    @JsonIgnore
     @OneToOne(mappedBy = "description",
             cascade = {CascadeType.DETACH, CascadeType.MERGE,
                     CascadeType.PERSIST, CascadeType.REFRESH})
     private PublicationEntity publication;
     private String title;
+    private String author;
     private String description;
     private Long issueNumber;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Date issueDate;
     private String coverImageLink;
 
     public DescriptionEntity(String title,
+                             String author,
+                             String description,
+                             Long issueNumber,
+                             String coverImageLink) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.issueNumber = issueNumber;
+        this.issueDate = new Date();
+        this.coverImageLink = coverImageLink;
+    }
+
+    public DescriptionEntity(String title,
+                             String author,
                              String description,
                              Long issueNumber,
                              Date issueDate,
                              String coverImageLink) {
         this.title = title;
+        this.author = author;
         this.description = description;
         this.issueNumber = issueNumber;
         this.issueDate = issueDate;
