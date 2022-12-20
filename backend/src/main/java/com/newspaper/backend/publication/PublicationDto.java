7c37c10185bdde6b628aae9b55ec741ab1ed29c3
EresK <e.kuular@g.nsu.ru>
2022-12-20 19:13:06 +0700

    delete dto

--- backend/src/main/java/com/newspaper/backend/publication/PublicationDto.java
+++
@@ -1,9 +0,0 @@
-package com.newspaper.backend.publication;
-
-import com.newspaper.backend.description.DescriptionDto;
-import lombok.Data;
-
-@Data
-public class PublicationDto {
-    private DescriptionDto descriptionDto;
-}
