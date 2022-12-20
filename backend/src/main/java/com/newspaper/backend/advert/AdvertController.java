ce4804245865fa9def89b63f0c0b9dc2097c3de3
Oleg Ivanov <o.ivanov@g.nsu.ru>
2022-12-20 21:51:40 +0700

    Create AdvertController.java

---
+++ backend/src/main/java/com/newspaper/backend/advert/AdvertController.java
@@ -0,0 +1,24 @@
+package com.newspaper.backend.advert;
+
+import com.newspaper.backend.publication.PublicationEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/adverts")
+public class AdvertController {
+    private final AdvertService advertService;
+    @PostMapping
+    public void postPublication(Authentication auth, @RequestBody AdvertEntity advert) {
+        advertService.createAdvert(auth, advert);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePublication(Authentication auth, @PathVariable Long id) {
+        advertService.deleteAdvert(auth, id);
+    }
+}
