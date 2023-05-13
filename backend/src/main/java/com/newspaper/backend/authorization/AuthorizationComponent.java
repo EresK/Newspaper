f75289f6dadf6fd47c86038d2636f2114a0f2b01
EresK <e.kuular@g.nsu.ru>
2023-05-14 01:54:57 +0700

    add check isOwnerOf for publication & advert

--- backend/src/main/java/com/newspaper/backend/authorization/AuthorizationComponent.java
+++ backend/src/main/java/com/newspaper/backend/authorization/AuthorizationComponent.java
@@ -1,15 +1,30 @@
 package com.newspaper.backend.authorization;
 
+import com.newspaper.backend.advert.AdvertEntity;
+import com.newspaper.backend.publication.PublicationEntity;
 import com.newspaper.backend.user.UserEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @Component("Auth")
-public class AuthorizationComponent implements IAuthorizationComponent {
-    public boolean isSameUser(UserEntity principal, Long id) {
+public class AuthorizationComponent {
+    public static boolean isSameUser(UserEntity principal, Long id) {
         return principal.getId().equals(id);
     }
 
-    public boolean isSameUser(UserEntity principal, String email) {
+    public static boolean isSameUser(UserEntity principal, String email) {
         return principal.getUsername().equals(email);
+    }
+
+    public static boolean isOwnerOf(UserEntity user, Object resource) {
+        if (resource instanceof PublicationEntity) {
+            return Objects.equals(user.getId(), ((PublicationEntity) resource).getOwner().getId());
+        }
+        else if (resource instanceof AdvertEntity) {
+            return Objects.equals(user.getId(), ((AdvertEntity) resource).getAdvertiser().getId());
+        }
+        else
+            return false;
     }
 }
