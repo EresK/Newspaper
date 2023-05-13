897bb2d2bdfaedf8237c40ad9798d3ca0e260da6
EresK <e.kuular@g.nsu.ru>
2023-05-14 01:58:27 +0700

    del some roles

--- backend/src/main/java/com/newspaper/backend/authorization/role/DefaultRole.java
+++ backend/src/main/java/com/newspaper/backend/authorization/role/DefaultRole.java
@@ -3,8 +3,6 @@ package com.newspaper.backend.authorization.role;
 import java.util.Optional;
 
 public enum DefaultRole {
-    ROLE_ADMIN("ROLE_ADMIN"),
-    ROLE_OWNER("ROLE_OWNER"),
     ROLE_EDITOR("ROLE_EDITOR"),
     ROLE_DESIGNER("ROLE_DESIGNER"),
     ROLE_ADVERTISER("ROLE_ADVERTISER");
@@ -17,8 +15,6 @@ public enum DefaultRole {
 
     public static Optional<DefaultRole> toRole(String roleName) {
         return switch (roleName.toUpperCase()) {
-            case "ROLE_ADMIN" -> Optional.of(ROLE_ADMIN);
-            case "ROLE_OWNER" -> Optional.of(ROLE_OWNER);
             case "ROLE_EDITOR" -> Optional.of(ROLE_EDITOR);
             case "ROLE_DESIGNER" -> Optional.of(ROLE_DESIGNER);
             case "ROLE_ADVERTISER" -> Optional.of(ROLE_ADVERTISER);
