1fb8e61de9862b3354c2cbb72e8160fc1be990fa
luchshiyDed <81702687+luchshiyDed@users.noreply.github.com>
2023-06-20 13:30:56 +0700

    Update SecurityConfiguration.java

--- backend/src/main/java/com/newspaper/backend/security/SecurityConfiguration.java
+++ backend/src/main/java/com/newspaper/backend/security/SecurityConfiguration.java
@@ -1,9 +1,11 @@
 package com.newspaper.backend.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.newspaper.backend.user.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,7 +21,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SecurityConfiguration implements WebMvcConfigurer {
     private final UserService userService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
